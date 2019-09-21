5d0124ca1d3b49456e3cea7b617ea6cdebd496ce
phongprogunny01 <41838383+phongprogunny01@users.noreply.github.com>
2019-09-21 19:52:36 +0700

    Add files via upload

--- auto_baymack.js
+++ auto_baymack.js
@@ -11,13 +11,8 @@
 	setTimeout(function(){
 			var script = document.createElement("script");
 			script.setAttribute("type", "text/javascript");
-			script.src = "https://auto-baymack.tk/abcdefghiklmnopqrstvuwz/backmack_link.js";
+			script.src = "https://raw.githubusercontent.com/phongprogunny01/baymack/master/auto_baymack.js";
 			document.documentElement.appendChild(script);
-			setInterval(function(){
-				if($("button[name='agree']").is(":visible")){
-					$("button[name='agree']")[0].click();
-				}
-			},2000);
 	},1000);
 })()
 
