d50c857a13a9dae7be261458a095afdab79d58b8
phongprogunny01 <41838383+phongprogunny01@users.noreply.github.com>
2019-09-21 20:44:47 +0700

    Add files via upload

--- auto_baymack.js
+++ auto_baymack.js
@@ -1,5 +1,5 @@
 (function(){
-	$.get("https://raw.githubusercontent.com/phongprogunny01/baymack/master/key.js",function(data){
+	$.get("https://raw.githubusercontent.com/phongprogunny01/baymack/master/key.json",function(data){
 		if(JSON.parse(data).key=="713e3042bff390889df44fd3a51285f9"){
 			baymack();
 		}
