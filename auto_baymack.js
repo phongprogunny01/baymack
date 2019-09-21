4ecef5b0d5df66ce87122652a52bbf93aee8a14d
phongprogunny01 <41838383+phongprogunny01@users.noreply.github.com>
2019-09-21 20:02:44 +0700

    Add files via upload

--- auto_baymack.js
+++ auto_baymack.js
@@ -1,19 +1,14 @@
-// ==UserScript==
-// @name         Auto baymack
-// @namespace    http://tampermonkey.net/
-// @version      999999999999999999999999999999999
-// @description  try to take over the world!
-// @author       TTP
-// @match        https://*
-// @grant        none
-// ==/UserScript==
 (function(){
-	setTimeout(function(){
-			var script = document.createElement("script");
-			script.setAttribute("type", "text/javascript");
-			script.src = "https://raw.githubusercontent.com/phongprogunny01/baymack/master/auto_baymack.js";
-			document.documentElement.appendChild(script);
-	},1000);
+	$.get("https://raw.githubusercontent.com/phongprogunny01/baymack/master/key.js",function(data){
+		if(JSON.parse(data).key=="713e3042bff390889df44fd3a51285f9"){
+			baymack();
+		}
+	});
+function baymack(){
+	setInterval(function(){
+		console.log("cc");
+	},2000);
+}
 })()
 
 
